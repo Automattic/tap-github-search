0c5778bb2e9846ebeba3b6b46a073a9c2a51fb8b
Mahangu Weerasinghe <mahangu@automattic.com>
2025-09-08 15:05:09 +0530

    Add support for base64 encoded config to avoid YARN shell env var issues

--- tap_github_search/tap.py
+++ tap_github_search/tap.py
@@ -18,10 +18,7 @@ class TapGitHubSearch(TapGitHub):
         search_cfg = os.environ.get("GITHUB_SEARCH_CONFIG")
         
         if search_cfg_b64:
-            try:
-                search_cfg = base64.b64decode(search_cfg_b64).decode("utf-8")
-            except Exception as e:
-                raise ValueError(f"Failed to decode TAP_GITHUB_SEARCH_STATS_SEARCH_B64: {e}")
+            search_cfg = base64.b64decode(search_cfg_b64).decode("utf-8")
         
         if not search_cfg and "search" not in self.config:
             raise ValueError("Provide search.* in config, set GITHUB_SEARCH_CONFIG, or set TAP_GITHUB_SEARCH_STATS_SEARCH_B64.")
